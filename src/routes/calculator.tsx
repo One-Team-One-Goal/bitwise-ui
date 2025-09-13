65f97c3ebc4b25e0bbf835b6d3ee890115eb7493
kwiruu <keiru277@gmail.com>
2025-09-13 18:39:53 +0800

    partial calculator

--- src/routes/calculator.tsx
+++ src/routes/calculator.tsx
@@ -1,5 +1,6 @@
 import CalculatorOutput from '@/components/CalculatorOutput'
 import CalculatorPanel from '@/components/CalculatorPanel'
+import FactoringDemo from '@/components/FactoringDemo'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/calculator')({
@@ -10,7 +11,7 @@ function RouteComponent() {
   return (
     <div className="m-auto mt-30 flex flex-col sm:flex-col md:flex-col lg:flex-row w-2/3 min-h-[80vh] gap-20 p-4 ">
       <CalculatorPanel />
-      <CalculatorOutput value="0" />
+      <FactoringDemo />
     </div>
   )
 }
--- src/routes/converter.tsx
+++ src/routes/converter.tsx
@@ -1,9 +1,15 @@
 import { createFileRoute } from '@tanstack/react-router'
+import FactoringDemo from '../components/FactoringDemo'
 
 export const Route = createFileRoute('/converter')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/converter"!</div>
+  return (
+    <div className="flex flex-col min-h-screen">
+      <div className='h-32 w-full'></div>
+      <FactoringDemo />
+    </div>
+  )
 }
